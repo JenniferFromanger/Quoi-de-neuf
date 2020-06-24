1721924806979b38e0b35f8ac1373ff2774fcd90
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-06-24 15:01:33 +0200

    initialize the project and the homepage

---
+++ quoi-de-neuf/src/components/Homepage.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Homepage.scss";
+function Homepage() {
+  const initialValues = { username: "", password: "" };
+  const [values, setValues] = useState(initialValues);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values);
+    setValues(initialValues);
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const newValues = { ...values, [name]: value };
+    setValues(newValues);
+  };
+  const { username, password } = values;
+  return (
+    <div className="App">
+      <h1>Rock your health</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            id="username"
+            name="username"
+            onChange={handleChange}
+            value={username}
+            placeholder="Enter your 
+         pseudonym"
+            required
+          ></input>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={handleChange}
+            value={password}
+            placeholder="Enter your 
+         password"
+            required
+          ></input>
+          <button type="submit">GO</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default Homepage;
