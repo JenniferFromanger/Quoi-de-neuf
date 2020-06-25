8fb8f769b49dd91963e95a0ddf4dfaa983fc3b26
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-06-25 15:25:32 +0200

    add picture on Form7

--- quoi-de-neuf/src/components/Form7.jsx
+++ quoi-de-neuf/src/components/Form7.jsx
@@ -11,27 +11,32 @@ export default function Form7() {
       <div className="form-header">
         <h1>MY DAILY CHECK-UP</h1>
       </div>
-      <p>Drunk any alcohool ?</p>
-      <Link to="/Report">
-        <button
-          className="yes"
-          type="button"
-          onClick={() =>
-            dispatch({ type: "ANSWER_DRINK", answer: true, points: -20 })
-          }
-        >
-          YES
-        </button>
-        <button
-          className="no"
-          type="button"
-          onClick={() =>
-            dispatch({ type: "ANSWER_DRINK", answer: true, points: 20 })
-          }
-        >
-          NO
-        </button>
-      </Link>
+      <div>
+        <img src="/liquor.png" alt="alcohol bottle" />
+      </div>
+      <div className="questions">
+        <p>Drunk any alcohool ?</p>
+        <Link to="/Report">
+          <button
+            className="yes"
+            type="button"
+            onClick={() =>
+              dispatch({ type: "ANSWER_DRINK", answer: true, points: -20 })
+            }
+          >
+            YES
+          </button>
+          <button
+            className="no"
+            type="button"
+            onClick={() =>
+              dispatch({ type: "ANSWER_DRINK", answer: true, points: 20 })
+            }
+          >
+            NO
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
